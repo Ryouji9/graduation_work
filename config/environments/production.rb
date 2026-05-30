2424cc15c055c4b33a576c65b5b9e46b92942dd7
Ryouji9 <ryoujiabe@gmail.com>
2026-05-30 18:30:12 +0900

    Renderホストを許可

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.hosts << "graduation-work-a1ro.onrender.com"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
