ad18785d4a9944b398ff7da9858a8cb3fd8ede7c
Ryouji9 <ryoujiabe@gmail.com>
2026-05-30 18:39:02 +0900

    application.rbにホスト設定を追加

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module App
   class Application < Rails::Application
+    config.hosts << "graduation-work-a1ro.onrender.com"
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
